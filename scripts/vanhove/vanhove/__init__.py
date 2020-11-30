7db505d5b84fad5bc8a835084517a2a2148ac89e
Emile de Bruyn <e.de.bruyn@fz-juelich.de>
2020-11-30 09:24:58 +0100

    Moved correct __init__.py to top-level directory.

---
+++ scripts/vanhove/vanhove/__init__.py
@@ -0,0 +1,8 @@
+"""
+This package provides facilities to calculate the dynamic correlation function G(r,t), also known as the van-Hove-function, by taking MDTraj trajectory objects together with MDTraj groups as input, and returning an array containing the function values at various times from t=0.
+"""
+
+from .src.vanhove import grt
+from .src.vanhove_cython import avg_grt as grt_cython
+from .src.plotting import plot_grt
+from .src.plotting import plot_map
--- scripts/vanhove/vanhove/src/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-This package provides facilities to calculate the dynamic correlation function G(r,t), also known as the van-Hove-function, by taking MDTraj trajectory objects together with MDTraj groups as input, and returning an array containing the function values at various times from t=0.
-"""
-
-from .vanhove import grt
-from .vanhove_cython import avg_grt as grt_cython
-from .plotting import plot_grt
-from .plotting import plot_map
