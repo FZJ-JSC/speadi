f10f7a1ba570c60cc462d518f9229429e3a18c73
Emile de Bruyn <e.de.bruyn@fz-juelich.de>
2020-12-02 09:48:14 +0100

    Added .__version__ attribute support for Python3.8 and higher.

--- scripts/vanhove/vanhove/__init__.py
+++ scripts/vanhove/vanhove/__init__.py
@@ -1,9 +1,13 @@
 """
 This package provides facilities to calculate the dynamic correlation function G(r,t), also known as the van-Hove-function, by taking MDTraj trajectory objects together with MDTraj groups as input, and returning an array containing the function values at various times from t=0.
 """
-import importlib_metadata
 
-__version__ = importlib_metadata.version('vanhove')
+try:
+    from importlib.metadata import version as get_pkg_version
+except ModuleNotFoundError:
+    from importlib_metadata import version as get_pkg_version
+
+__version__ = get_pkg_version('vanhove')
 
 from .src.vanhove import grt
 from .src.vanhove_cython import avg_grt as grt_cython
