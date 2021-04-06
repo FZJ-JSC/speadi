ece0be4ca9373d717c472fb26d39276067f7debf
Emile de Bruyn <e.de.bruyn@fz-juelich.de>
2021-04-06 10:17:34 +0200

    Re-added prange over g2 (solvent or ions).

--- scripts/time_resolved_rdf/time_resolved_rdf/src/time_resolved_rdf.py
+++ scripts/time_resolved_rdf/time_resolved_rdf/src/time_resolved_rdf.py
@@ -216,7 +216,7 @@ def _compute_grt_numba(rt_array, chunk_unitcell_volumes, r_range, nbins, raw_cou
 @jit
 def _opt_append_grts(g_rts, n, xyz, g1, g2, g1_lens, g2_lens, cuvec, cuvol, r_range, nbins, raw_counts):
     for i in prange(g1.shape[0]):
-        for j in range(g2.shape[0]):
+        for j in prange(g2.shape[0]):
             rt_array = _compute_rt_mic_numba(xyz, g1[i][:g1_lens[i]], g2[j][:g2_lens[j]], cuvec)
             g_rts[i,j,n] += _compute_grt_numba(rt_array, cuvol, r_range, nbins, raw_counts)
     return g_rts
