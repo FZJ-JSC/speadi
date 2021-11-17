9e4348761b14ff5d40d96f1469faccd12f17cc93
Emile de Bruyn <e.de.bruyn@fz-juelich.de>
2021-11-17 21:03:14 +0100

    Fixed error in TRRDF norming vs VHF (and MDTraj's `compute_rdf`).

--- mdenvironment/time_resolved_rdf/jit/histogram_distances.py
+++ mdenvironment/time_resolved_rdf/jit/histogram_distances.py
@@ -30,23 +30,17 @@ def _compute_grt_numba(rt_array, window_unitcell_volumes, r_range, nbins, raw_co
     Ni = rt_array.shape[1]
     Nj = rt_array.shape[2]
     n_frames = rt_array.shape[0]
-    g_rt = np.empty(nbins, dtype=float64)
     edges = np.linspace(r_range[0], r_range[1], nbins + 1)
-    # for t in prange(n_frames):
-    # g_r, edges = np.histogram(rt_array[t], range=r_range, bins=bins)
-    # g_rt[t] = g_r
-    # g_rt[t] = _histogram(rt_array[t], edges)
     g_rt = _histogram(rt_array, edges)
 
     if raw_counts:
         # No normalisation w.r.t volume and particle density
         g_rt = g_rt / Ni / n_frames
     else:
-        # r = 0.5 * (edges[1:] + edges[:-1])
-        r_vol = 4.0 * np.pi * np.power(edges[1:], 2) * (edges[1:] - edges[:-1])
+        r_vol = 4.0 / 3.0 * np.pi * (np.power(edges[1:], 3) - np.power(edges[:-1], 3))
         Nj_density = Nj / window_unitcell_volumes.mean()
 
-        # Use normal RDF norming for each time step
+        # Use normal RDF norming over each time step
         norm = Nj_density * r_vol * Ni
         g_rt = g_rt / norm / n_frames
 
