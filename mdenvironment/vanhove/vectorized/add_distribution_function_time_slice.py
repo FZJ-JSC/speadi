5ac8d16c00bf28cb96b3ff9023265281eef0f7b8
Emile de Bruyn <e.de.bruyn@fz-juelich.de>
2022-03-09 23:04:16 +0100

    Added general MIC for non-jitted function.

--- mdenvironment/vanhove/vectorized/add_distribution_function_time_slice.py
+++ mdenvironment/vanhove/vectorized/add_distribution_function_time_slice.py
@@ -1,11 +1,11 @@
 from .histogram_distances import _compute_G_distinct, _compute_G_self
-from .time_distance_matrix import _compute_rt_mic, _compute_rt
+from .time_distance_matrix import _compute_rt_ortho_mic, _compute_rt_general_mic
 
 
-def _mic_append_Grts(G_self, G_distinct, xyz, g1, g2, cuvec, cuvol, r_range, nbins):
+def _plain_append_Grts_ortho_mic(G_self, G_distinct, xyz, g1, g2, cuvec, cuvol, r_range, nbins):
     for i, sub_g1 in enumerate(g1):
         for j, sub_g2 in enumerate(g2):
-            rt_self, rt_distinct = _compute_rt_mic(xyz, sub_g1, sub_g2, cuvec)
+            rt_self, rt_distinct = _compute_rt_ortho_mic(xyz, sub_g1, sub_g2, cuvec)
             r, distances = _compute_G_distinct(rt_distinct, cuvol, r_range, nbins)
             G_distinct[i, j] += distances
         r, self_distance = _compute_G_self(rt_self, cuvol, r_range, nbins)
@@ -14,10 +14,10 @@ def _mic_append_Grts(G_self, G_distinct, xyz, g1, g2, cuvec, cuvol, r_range, nbi
     return r, G_self, G_distinct
 
 
-def _plain_append_Grts(G_self, G_distinct, xyz, g1, g2, cuvol, r_range, nbins):
+def _plain_append_Grts_general_mic(G_self, G_distinct, xyz, g1, g2, cuvec, cuvol, r_range, nbins):
     for i, sub_g1 in enumerate(g1):
         for j, sub_g2 in enumerate(g2):
-            rt_self, rt_distinct = _compute_rt(xyz, sub_g1, sub_g2)
+            rt_self, rt_distinct = _compute_rt_general_mic(xyz, sub_g1, sub_g2, cuvec)
             r, distances = _compute_G_distinct(rt_distinct, cuvol, r_range, nbins)
             G_distinct[i, j] += distances
         r, self_distance = _compute_G_self(rt_self, cuvol, r_range, nbins)
--- mdenvironment/vanhove/vectorized/time_distance_matrix.py
+++ mdenvironment/vanhove/vectorized/time_distance_matrix.py
@@ -1,20 +1,32 @@
 import numpy as np
-from scipy.spatial.distance import cdist
 
 
-def _compute_rt(window, g1, g2):
-    rt_distinct = np.empty((window.shape[0], g1.shape[0], g2.shape[0]), dtype=np.float32)
-    rt_self = np.empty((window.shape[0], g1.shape[0]), dtype=np.float32)
-    r01 = window[0, g1]
+def _compute_rt_general_mic(window, g1, g2, box_vectors):
+    rt_distinct = np.zeros((window.shape[0], g1.shape[0], g2.shape[0]), dtype=np.float32)
+    rt_self = np.zeros((window.shape[0], g1.shape[0]), dtype=np.float32)
+    r1 = window[0, g1]
+    xyz = window[:, g2]
+
     for t in range(rt_distinct.shape[0]):
-        rt_distinct[t] = cdist(r01, window[t, g2])
+        bv_inv = np.linalg.inv(box_vectors[t])
+
+        s1 = bv_inv * r1[:, :, np.newaxis]
+        s2 = bv_inv * xyz[t, :, :, np.newaxis]
+        s12 = _grid_sub(s1, s2)
+        s12 -= np.rint(s12)
+        r12 = box_vectors[t] * s12
+        rt_diag = np.diagonal(r12.T)
+        rt_sq = np.sum(rt_diag**2, axis=2)
+        rt_distinct[t] = np.sqrt(rt_sq)
+
+        # remove self interaction part of G(r,t)
         rt_self[t] = np.diagonal(rt_distinct[t])
         np.fill_diagonal(rt_distinct[t], 9999.0)
 
     return rt_self, rt_distinct
 
 
-def _compute_rt_mic(window, g1, g2, box_vectors):
+def _compute_rt_ortho_mic(window, g1, g2, box_vectors):
     bv = np.diag(box_vectors.mean(axis=0))
 
     rt_distinct = np.empty((window.shape[0], g1.shape[0], g2.shape[0]), dtype=np.float32)
@@ -29,6 +41,7 @@ def _compute_rt_mic(window, g1, g2, box_vectors):
         r12 = r12.sum(axis=2)
         r12 = np.sqrt(r12)
         rt_distinct[t] = r12.T
+
         # remove self interaction part of G(r,t)
         rt_self[t] = np.diagonal(rt_distinct[t])
         np.fill_diagonal(rt_distinct[t], 9999.0)
