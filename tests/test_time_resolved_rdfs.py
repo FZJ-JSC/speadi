d8b0fe1ef5d55af8349857de338be7f123487d61
Emile de Bruyn <e.de.bruyn@fz-juelich.de>
2022-09-18 23:57:54 +0200

    Added test to check for inconsistencies with unequal reference groups.

--- tests/test_time_resolved_rdfs.py
+++ tests/test_time_resolved_rdfs.py
@@ -3,23 +3,39 @@ from itertools import product
 
 set_num_threads(2)
 
-refs = ['NA']
+ref = ['NA']
+refs = [('NA', 'CL'), ('O', 'NA')]
 pbc = ['ortho', 'general']
 JAX_AVAILABLE = [True, False]
-NUMBA_AVAILABLE = [True, False]
-parameter_sets = list(product(refs, pbc, JAX_AVAILABLE, NUMBA_AVAILABLE))
+# NUMBA_AVAILABLE = [True, False]
+NUMBA_AVAILABLE = [True]
+single_ref_params = list(product(ref, pbc, JAX_AVAILABLE, NUMBA_AVAILABLE))
+double_ref_params = list(product(refs, pbc, JAX_AVAILABLE, NUMBA_AVAILABLE))
 
 
 def idfn(args):
     return f'ref: {args[0]}, pbc: {args[1]}, JAX_AVAILABLE: {args[2]}, NUMBA_AVAILABLE: {args[3]}'
 
 
-@pytest.fixture(scope='module', params=parameter_sets, ids=idfn)
+@pytest.fixture(scope='module', params=single_ref_params, ids=idfn)
 def mde_rdf(request, paths, nacl_top, mdtraj_groups):
     mde.JAX_AVAILABLE = request.param[2]
     mde.NUMBA_AVAILABLE = request.param[3]
 
     r, grt = mde.trrdf(paths['traj_path'], mdtraj_groups[request.param[0]], mdtraj_groups['O'], top=nacl_top,
+                       pbc=request.param[1], n_windows=20, window_size=10, skip=0, stride=1, r_range=(0.0, 1.2),
+                       nbins=120)
+    return r, grt, request.param[0]
+
+
+@pytest.fixture(scope='module', params=double_ref_params, ids=idfn)
+def double_refs_mde_rdf(request, paths, nacl_top, mdtraj_groups):
+    mde.JAX_AVAILABLE = request.param[2]
+    mde.NUMBA_AVAILABLE = request.param[3]
+    refs = request.param[0]
+    ref_groups = [mdtraj_groups[ref] for ref in refs]
+
+    r, grt = mde.trrdf(paths['traj_path'], ref_groups, mdtraj_groups['O'], top=nacl_top,
                        pbc=request.param[1], n_windows=20, window_size=10, skip=0, stride=1, r_range=(0.0, 1.2),
                        nbins=120)
     return r, grt, request.param[0]
@@ -48,9 +64,19 @@ def test_rdf_results_gmx(mde_rdf, gmx_rdf):
     np.testing.assert_allclose(gr, gmx_gr[ref], rtol=5e-2)
 
 
-def test_rdf_results_mdtraj(mde_rdf, mdtraj_rdf):
+def test_single_rdf_results_mdtraj(mde_rdf, mdtraj_rdf):
     _, grt, ref = mde_rdf
     _, mdtraj_gr = mdtraj_rdf
     gr = np.mean(grt, axis=(0,1,2))
 
     np.testing.assert_allclose(gr, mdtraj_gr[ref], rtol=5e-2)
+
+
+def test_double_rdf_results_mdtraj(double_refs_mde_rdf, mdtraj_rdf):
+    _, grt, refs = double_refs_mde_rdf
+    _, mdtraj_gr = mdtraj_rdf
+    gr1 = np.mean(grt[0], axis=(0,1))
+    gr2 = np.mean(grt[1], axis=(0,1))
+
+    np.testing.assert_allclose(gr1, mdtraj_gr[refs[0]], rtol=5e-2)
+    np.testing.assert_allclose(gr2, mdtraj_gr[refs[1]], rtol=5e-2)
