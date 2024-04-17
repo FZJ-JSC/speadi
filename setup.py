d263511b913f55a5b53bc1bec3e1a9c613d68746
Emile de Bruyn <44635009+EmileDeBruyn@users.noreply.github.com>
2024-04-17 16:51:13 +0200

    Update version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from pathlib import Path
 
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 this_directory = Path(__file__).parent
 long_description = (this_directory / 'README.md').read_text()
