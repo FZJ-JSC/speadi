e8621e426471e80578587403df6297933a18c53a
Emile de Bruyn <44635009+EmileDeBruyn@users.noreply.github.com>
2024-04-17 16:21:57 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,12 @@ setup(
     version=__version__,
     packages=find_packages(),
     author='Emile de Bruyn',
-    author_email='e.de.bruyn@fz-juelich.de'
+    author_email='e.de.bruyn@fz-juelich.de',
+    description="""
+    A Python package that aims to characterise the dynamics of local chemical environments
+    from Molecular Dynamics trajectories of proteins and other biomolecules.
+    """,
+    long_description_content_type="text/markdown",
+    long_description=README,
+    url="https://github.com/FZJ-JSC/speadi",
 )
