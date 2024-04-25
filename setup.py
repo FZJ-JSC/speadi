006f3d073626aaad443a8f8643b03b9732fe2555
emiledebruyn <emile.de.bruyn@rwth-aachen.de>
2024-04-25 15:01:39 +0200

    Bumped version to v.1.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from pathlib import Path
 
 
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 
 this_directory = Path(__file__).parent
 long_description = (this_directory / 'README.md').read_text()
